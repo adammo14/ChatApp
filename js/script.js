8e59a90be94d9c98e413af39e146d71f574d4fa1
Adam Chmielewski <adammo14@hotmail.com>
2018-03-20 13:56:20 +0000

    Added HotJar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-function swapStyleSheetNew(){
-	document.body.style.backgroundColor = "#AA0000";
-  console.log('it works');
-}
-
-function swapStyleSheetOld(){
-	document.getElementById('1').style.color = white;
-  console.log('it works');
-}
-
-function changeStylesheet(newstylesheet){
-    document.getElementById('pagestyle').setAttribute('href', newstylesheet);
-}
+//Hotjar Tracking Code for 'https://chatappadam.herokuapp.com/''
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:820861,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
 function formatAMPM(date) {
     var hours = date.getHours();
